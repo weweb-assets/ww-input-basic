fe682d8757ead1c9670f06f14d6e7706be69467a
Damien DURO <damien@weweb.io>
2021-01-19 16:11:59 +0100

    form input v1

--- src/wwObject.vue
+++ src/wwObject.vue
@@ -1,7 +1,8 @@
 <template>
     <input
+        v-if="content.config"
         class="ww-form-input"
-        :class="{ 'ww-editing': isEditing }"
+        :class="{ editing: isEditing }"
         :type="content.config.type"
         :name="content.config.name"
         :required="content.config.required"
@@ -12,10 +13,6 @@
 </template>
 
 <script>
-/* wwEditor:start */
-import openPopup from './popups';
-/* wwEditor:end */
-
 export default {
     name: '__COMPONENT_NAME__',
     props: {
@@ -27,19 +24,67 @@ export default {
     wwDefaultContent: {
         config: {
             type: 'text',
-            name: 'Name',
+            name: '',
             required: true,
             pattern: '.*',
-            placeholder: {
-                fr: 'Nom',
-                en: 'Name',
-            },
+            placeholder: {},
         },
         style: {
             color: 'black',
-            fontSize: 1,
+            fontSize: '15px',
         },
     },
+    /* wwEditor:start */
+    wwEditorConfiguration() {
+        return {
+            settingsOptions: {
+                name: {
+                    path: 'config.name',
+                    label: { en: 'Name', fr: 'fr' },
+                    type: 'Text',
+                    options: {
+                        placeholder: 'Name',
+                    },
+                },
+                required: {
+                    path: 'config.required',
+                    label: { en: 'Required', fr: 'Requis' },
+                    type: 'OnOff',
+                },
+                type: {
+                    path: 'config.type',
+                    label: { en: 'Input type', fr: 'fr' },
+                    type: 'TextSelect',
+                    options: {
+                        options: [
+                            { value: 'text', label: { en: 'Text', fr: 'Texte' } },
+                            { value: 'email', label: { en: 'Email', fr: 'Email' } },
+                            { value: 'password', label: { en: 'Password', fr: 'Mot de passe' } },
+                            { value: 'tel', label: { en: 'Phone', fr: 'Téléphone' } },
+                            { value: 'number', label: { en: 'Number', fr: 'Nombre' } },
+                            { value: 'date', label: { en: 'Date', fr: 'Date' } },
+                            { value: 'time', label: { en: 'Time', fr: 'Heure' } },
+                        ],
+                    },
+                },
+                ...(function () {
+                    const placeholders = {};
+                    for (const lang of wwLib.$store.getters['websiteData/getPage'].langs) {
+                        placeholders[`placeholder_${lang}`] = {
+                            path: `config.placeholder.${lang}`,
+                            label: { en: `placeholder (${lang})`, fr: 'fr' },
+                            type: 'Text',
+                            options: {
+                                placeholder: 'firstname',
+                            },
+                        };
+                    }
+                    return placeholders;
+                })(),
+            },
+        };
+    },
+    /* wwEditor:end */
     data() {
         return {
             wwLang: wwLib.wwLang,
@@ -47,23 +92,20 @@ export default {
     },
     computed: {
         isEditing() {
-            return this.wwEditorState.editMode === wwLib.wwSectionHelper.EDIT_MODES.CONTENT;
+            /* wwEditor:start */
+            return this.wwEditorState.editMode === wwLib.wwEditorHelper.EDIT_MODES.EDITION;
+            /* wwEditor:end */
+            // eslint-disable-next-line no-unreachable
+            return false;
         },
         style() {
+            if (!this.content || !this.content.style) return {};
             return {
                 color: this.content.style.color || 'black',
-                fontSize: `${this.content.style.fontSize || 1}rem`,
+                fontSize: `${this.content.style.fontSize || '1rem'}`,
             };
         },
     },
-    /* wwEditor:start */
-    methods: {
-        async setOptions() {
-            const result = await openPopup(this.content);
-            if (result) this.$emit('update', result);
-        },
-    },
-    /* wwEditor:end */
 };
 </script>
 
@@ -72,12 +114,13 @@ export default {
     width: 100%;
     outline: none;
     font-family: inherit;
+    border: inherit;
     &::placeholder {
         color: inherit;
         opacity: 0.7;
     }
     /* wwEditor:start */
-    &.ww-editing {
+    &.editing {
         pointer-events: none;
     }
     /* wwEditor:end */

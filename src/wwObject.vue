d9f6e9074ce76f9f4c1c080860077b6a5d863f81
Damien DURO <damien@weweb.io>
2021-01-21 15:28:50 +0100

    fix isEditing

--- src/wwObject.vue
+++ src/wwObject.vue
@@ -91,7 +91,7 @@ export default {
     computed: {
         isEditing() {
             /* wwEditor:start */
-            return this.wwEditorState.isSelected;
+            return this.wwEditorState.editMode === wwLib.wwSectionHelper.EDIT_MODES.CONTENT;
             /* wwEditor:end */
             // eslint-disable-next-line no-unreachable
             return false;

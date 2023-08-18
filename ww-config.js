f139e8a23f8e51dcbcef35b548a9c18692f8f4d8
Alexis JC <alexisjc.dev@gmail.com>
2023-08-18 15:48:45 +0200

    fix: wrong hidden condition

--- ww-config.js
+++ ww-config.js
@@ -276,7 +276,7 @@ export default {
                     : { min: 1, max: 100, step: 1 },
             defaultValue: 1,
             bindable: true,
-            hidden: content => content.type !== 'decimal' && content.type !== 'number' && content.type !== 'time',
+            hidden: content => content.type !== 'decimal' && content.type !== 'number',
         },
         timePrecision: {
             label: { en: 'Time precision', fr: 'Time precision' },

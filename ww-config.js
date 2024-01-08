66fdd7ba461d461b15deeb647c81297af257de26
luka-weweb <luka@weweb.io>
2024-01-08 12:13:29 +0100

    format

--- ww-config.js
+++ ww-config.js
@@ -8,7 +8,6 @@ export default {
         icon: 'text-input',
         customSettingsPropertiesOrder: [
             'value',
-            
             ['placeholder'],
             ['readonly', 'required'],
             ['debounce', 'debounceDelay'],
